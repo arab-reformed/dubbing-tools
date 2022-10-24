5c54c163eb35184ed9f397f059c38ac2de1520ea
John R. Supplee <john@supplee.com>
2022-10-24 15:31:33 +0200

    Updates to find-slides
    * find exact start and end frame of slides
    * create JSON files with frame data

--- find-slides.py
+++ find-slides.py
@@ -7,19 +7,29 @@ import sys
 import os
 # from skimage.measure import compare_ssim as ssim
 from skimage.metrics import structural_similarity as ssim
+import json
 
 
-def cmd(video_file: str, output_path: str, debug: bool = False, exact: bool = False):
-    
+def write_json(output_path: str, stills: list):
+    with open(os.path.join(output_path, 'still-frames.json'), 'w') as file:
+        json.dump({'stills': stills}, file, indent=2)
+
+
+def cmd(video_file: str, output_path: str, debug: bool = False, skip: int = 20, similarity: float = 0.95):
+
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
     capture = cv2.VideoCapture(video_file)
-    
+
     frame_num = 0
     last_frame = None
     frame_start = None
     frame_end = None
+    previous = {}
+    current = {}
+    stills = []
+    write_json(output_path, stills)
 
     while (True):
 
@@ -28,20 +38,42 @@ def cmd(video_file: str, output_path: str, debug: bool = False, exact: bool = Fa
         if not success:
             break
 
-        if frame_num % 20 == 0:
+        current[frame_num] = frame
+
+        if frame_num % skip == 0:
             # print(f'Frame: {frame_num}', file=sys.stderr)
             try:
                 if last_frame is None:
                     last_frame = frame
 
-                elif ssim(frame, last_frame, multichannel=True) > 0.90:  #, gaussian_weights=True, win_size=None, sigma=1.5, use_sample_covariance=False) > 0.7:
+                elif ssim(frame, last_frame, multichannel=True) > similarity:  #, gaussian_weights=True, win_size=None, sigma=1.5, use_sample_covariance=False) > 0.7:
                     if frame_start is None:
                         frame_start = frame_num
+                        for n in previous:
+                            if ssim(frame, previous[n], multichannel=True) > similarity:
+                                frame_start = n
+                                break
+                        stills.append({
+                            'frame_start': frame_start,
+                            'frame_end': frame_start,
+                        })
+
                     frame_end = frame_num
-                
+                    stills[-1]['frame_end'] = frame_end
+
                 else:
-                    if frame_start is not None and frame_end - frame_start >= 20:
-                        cv2.imwrite(f'{output_path}/{frame_start}-{frame_end}.jpg', last_frame)
+                    if frame_start is not None and frame_end - frame_start >= skip:
+                        for n in current:
+                            if ssim(last_frame, current[n], multichannel=True) > similarity:
+                                frame_end = n
+                            else:
+                                break
+
+                        stills[-1]['frame_end'] = frame_end
+                        stills[-1]['image_file'] = f'slide-{str(len(stills)).rjust(3, "0")}.jpg'
+                        cv2.imwrite(os.path.join(output_path, stills[-1]['image_file']), last_frame)
+                        write_json(output_path, stills)
+
                         frame_start = None
                         frame_end = None
 
@@ -50,6 +82,8 @@ def cmd(video_file: str, output_path: str, debug: bool = False, exact: bool = Fa
                 raise exc
 
             last_frame = frame
+            previous = current
+            current = {}
 
         frame_num += 1
 
